78cdcfbe84640a2eae97421da0f6325decedea67
harnath <harnath.atmakuri@algonox.com>
2020-04-17 16:16:43 +0530

    Url change

--- runserver.py
+++ runserver.py
@@ -6,7 +6,7 @@ from os import environ
 from __init__ import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('SERVER_HOST', 'https://health-tracker.azurewebsites.net')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
